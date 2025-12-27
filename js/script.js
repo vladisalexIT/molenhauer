e155782b5baa79d39c870d4326b9fc37fc66eaf8
vladisalex-it <vl4disalex@gmail.com>
2025-12-27 23:58:13 +0300

    feat: add services-slider section with interaction effects

---
+++ js/script.js
@@ -0,0 +1,9 @@
+
+    var swiper = new Swiper(".our-expertise__slider", {
+      slidesPerView: 6,
+      spaceBetween: 0,
+      pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+      },
+    });
